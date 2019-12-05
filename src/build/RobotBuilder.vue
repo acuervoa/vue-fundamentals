bdbe9d8721d68a982df99733c94994a0c673c65d
acuervoa <kuerbo@gmail.com>
2019-12-05 16:52:50 +0100

    Using Actions to Save data to an API

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -59,7 +59,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('getParts')
+    this.$store.dispatch('getParts');
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -102,7 +102,7 @@ export default {
         + robot.torso.cost
         + robot.rightArm.cost
         + robot.base.cost;
-      this.$store.commit('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
       this.addedToCart = true;
     },
   },
