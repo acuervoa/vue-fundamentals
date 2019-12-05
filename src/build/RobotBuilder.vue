8e682b7d37696b07eec5f63dda9db86e7919765f
acuervoa <kuerbo@gmail.com>
2019-12-05 16:58:31 +0100

    Returning promises from Actions

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -102,7 +102,8 @@ export default {
         + robot.torso.cost
         + robot.rightArm.cost
         + robot.base.cost;
-      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }));
+      this.$store.dispatch('addRobotToCart', Object.assign({}, robot, { cost }))
+        .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
   },
