b9542673c8d1165cdf689faaf4bdbc7abf2d543a
acuervoa <kuerbo@gmail.com>
2019-12-06 14:13:44 +0100

    Making Directives Available Globally

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,10 @@ import Vuex from 'vuex';
 
 import robotsModule from './modules/robots';
 import usersModule from './modules/users';
+import pinDirective from '../shared/pin-directive';
 
 Vue.use(Vuex);
+Vue.directive('pin', pinDirective);
 
 export default new Vuex.Store({
   modules: {
